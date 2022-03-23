6978803211010baea3937608dc8fb2dc2fbb898c
Markus Pedersen <pedersen140697@gmail.com>
2022-03-23 16:13:38 +0100

    klasse User og userDAO(implementert DAO interface) med ferdig metoder for nødvendig querys. Oppdatert persistence.xml filen(lagt til persistence-unit name=userDB)

--- src/main/java/classes/User.java
+++ src/main/java/classes/User.java
@@ -1,5 +1,53 @@
 package classes;
 
-public class User {
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(schema="yazy")
+public class User {
+	
+	@Id
+	private String username; 
+	private String email; 
+	@Embedded
+	private Password password;
+	
+	public User() {
+		
+	}
+	
+	public User(String username, String email, Password password) {
+		super(); 
+		this.username = username; 
+		this.email = email; 
+		this.password = password; 
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Password getPassword() {
+		return password;
+	}
+
+	public void setPassword(Password password) {
+		this.password = password;
+	}
+	
 }
--- src/main/java/utils/UserDAO.java
+++ src/main/java/utils/UserDAO.java
@@ -1,6 +1,36 @@
 package utils;
 
+import java.util.List;
 
-public class UserDAO {
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+
+import classes.User;
+
+@Stateless
+public class UserDAO implements DAO{
+	
+	private EntityManager em; 
+	
+
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return em.createQuery("SELECT s from users s ", User.class).getResultList();
+	}
+
+	@Override
+	public void addNewUser(User user) {
+		// TODO Auto-generated method stub
+		em.persist(user);
+		
+	}
+
+	@Override
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return em.find(User.class, username); 
+	}
 
 }
