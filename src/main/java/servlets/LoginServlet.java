80e05fe5a0576743b22c3995b94f4b4a4b03942a
Matias <h593308@stud.hvl.no>
2022-03-23 16:46:29 +0100

    implementer deler av loginServlet

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,11 +1,17 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import classes.User;
+import utils.PasswordUtil;
+import utils.UserDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,6 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private UserDAO userdao;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,8 +35,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("Login.jsp").forward(request, response);
 	}
 
 	/**
@@ -35,7 +43,14 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		User user = userdao.getUser(username);
+		
+		if (PasswordUtil.validateWithSalt(password, user.getPassword().getSalt(), user.getPassword().getHash())) {
+			response.sendRedirect("LoginServlet");
+		}
 	}
 
 }
