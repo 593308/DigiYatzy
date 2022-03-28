719ed70a60aa73fba7d618443e647612508bf7cc
Matias <h593308@stud.hvl.no>
2022-03-28 13:16:10 +0200

    opprettet et par klasser for spillogikken

--- src/main/java/gameClasses/Player.java
+++ src/main/java/gameClasses/Player.java
@@ -1,5 +1,34 @@
 package gameClasses;
 
-public class Player {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
 
+import gameUtils.PLayerState;
+
+@Entity
+@IdClass(PlayerId.class)
+public class Player {
+	
+	@Id
+	private String username;
+	private int gameId;
+	private PlayerState playerstate;
+	private int onesScore;
+	private int twosScore;
+	private int threesScore;
+	private int foursScore;
+	private int fivesScore;
+	private int sixesScore;
+	private int bonusScore;
+	private int onePairScore;
+	private int twoPairsScore;
+	private int threeOfAKindScore;
+	private int fourOfAKindScore;
+	private int smallStraightScore;
+	private int largeStraightScore;
+	private int fullHouseScore;
+	private int chanceScore;
+	private int yatzyScore;
+	private int totalScore;
 }
---
+++ src/main/java/gameClasses/PlayerId.java
@@ -0,0 +1,13 @@
+package gameClasses;
+
+public class PlayerId {
+	
+	private String username;
+	private int gameId;
+	
+	public PlayerId(String username, int gameId) {
+		this.username = username;
+		this.gameId = gameId;
+	}
+
+}
