4429773a91c98efafc47f48247e44277f94786b0
h584964 <584964@stud.hvl.no>
2022-03-30 13:36:52 +0200

    GameServlet

--- src/main/java/servlets/YatzyGameServlet.java
+++ src/main/java/servlets/YatzyGameServlet.java
@@ -26,7 +26,7 @@ public class YatzyGameServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("WEB-INF/Game.jsp").forward(request, response);
 	}
 
 	/**
